719889188bf58ad3d424187a5503d95b5077ebc1
Lawrence <theinversesquarelaw@gmail.com>
2024-07-01 20:40:01 -0700

    Finished question 8.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { products } = require('./data');
 
 // Question 4
 app.use(express.static('./public'));
@@ -7,6 +8,11 @@ app.use(express.static('./public'));
 // Question 7
 app.get('/api/v1/test', (req, res) => {
     res.json({ message: "It worked!" });
+});
+
+// Question 8 - if you mis-type and leave out the leading slash, the route will not work!!!
+app.get('/api/v1/products', (req, res) => {
+    res.json(products);
 });
 
 // Question 6
