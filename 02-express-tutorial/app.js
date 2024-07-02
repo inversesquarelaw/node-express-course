306047d3c0a0c3975c3da4ae25ccd39f663b51c2
Lawrence <theinversesquarelaw@gmail.com>
2024-07-01 20:30:00 -0700

    Finished question 6.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,7 +4,10 @@ const app = express();
 // Question 4
 app.use(express.static('./public'));
 
-
+// Question 6
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>404! The page you requested is not found!</h1>');
+});
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000...');
